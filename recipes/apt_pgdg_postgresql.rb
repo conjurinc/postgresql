a9160b06b80934018c4deac431e3d7df480e50b3
Dustin Collins <dustinrcollins@gmail.com>
2015-07-08 22:29:56 -0400

    Add trusty 14.04 as a target for install postgres

--- recipes/apt_pgdg_postgresql.rb
+++ recipes/apt_pgdg_postgresql.rb
@@ -1,4 +1,4 @@
-if not %w(etch lenny lucid precise sid squeeze wheezy).include?(release = node['postgresql']['pgdg']['release_apt_codename'])
+if not %w(etch lenny lucid precise sid squeeze wheezy trusty).include?(release = node['postgresql']['pgdg']['release_apt_codename'])
   raise "Not supported release by PGDG apt repository: #{release}"
 end
 
